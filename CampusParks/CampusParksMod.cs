c89f4db63a4732357669049f7785a12516df9b26
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2022-02-01 21:04:34 +0100

    Bump version to 1.0.2

--- CampusParks/CampusParksMod.cs
+++ CampusParks/CampusParksMod.cs
@@ -5,7 +5,7 @@ namespace CampusParks
 {
     public class ChangeLoadingImageMod : IUserMod
     {
-        public string Name => "Campus Parks";
+        public string Name => "Campus Parks r1.0.2";
 
         public string Description => "Allows to place parks on pedestrian paths in campuses";
 
